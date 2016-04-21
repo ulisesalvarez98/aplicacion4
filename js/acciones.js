766b35c3aeeac4e8307a9ca389e0da9493279f2b
ulisesalvarez98 <ulisesalvarezcorio3h@gmail.com>
2016-04-21 15:35:12 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function (e){
 	document.addEventListener("deviceready", function(){
-		$('#disp table td').eq(3).text(decive.model);
-		$('#disp table td').eq(5).text(decive.cordova);
-		$('#disp table td').eq(7).text(decive.platform);
-		$('#disp table td').eq(9).text(decive.version);
-		$('#disp table td').eq(11).text(decive.uuid);
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 		document.addEventListener("pause", function(){
 			describrehistoria ('La app se pausó');
 		}, false);//pause
